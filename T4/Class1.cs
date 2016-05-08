24f7c6cfee91086cded866d0f2649e5fe76029cd
yummyguo <yummyguo@hotmail.com>
2016-05-08 11:35:47 +0800

    test

---
+++ T4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class Class1
+    {
+        string result = new RuntimeTextTemplate1().TransformText(); 
+        
+
+    }
+}
