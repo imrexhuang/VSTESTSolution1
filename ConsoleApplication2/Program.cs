00936bc94ffd7d427b03d447d91e14209283736e
rex <im@rex.tw>
2016-05-08 13:50:04 +0800

    test

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello 1");
+            String aa = "AAA";
+            Debug.WriteLine("hello debug");
+                
+            try {
+                Console.WriteLine("hello 2");
+                //https://msdn.microsoft.com/en-us/library/d762ceey.aspx
+                Debug.Assert(aa != "AAA" ,"TEST....");
+                //Debug.Assert(aa == "AAA", "TEST....");
+                //Debug.Assert(aa != "AAA");
+                //Trace.Assert(aa != null, "Type parameter is null");
+                //Debug.Fail("fail..");
+            }
+            catch (Exception ex) {
+                ex.ToString();
+            }
+        }
+    }
+}
